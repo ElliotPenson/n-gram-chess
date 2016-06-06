259a3c82e669191756aeef5fc31346074ed96030
Elliot Penson <elliotpenson@gmail.com>
2016-06-06 19:41:41 -0400

    Implement functions that determine move likelihoods

--- n-gram-chess.lisp
+++ n-gram-chess.lisp
@@ -34,3 +34,32 @@
                  until (null tag-pairs)
                  do (count-n-grams moves count-map n)))))
     count-map))
+
+(defun move-probability (move previous-moves count-map)
+  "Find the likelihood of a chess move given a list of previous moves.
+   Calculated as P(move|previous-moves) = P(previous-moves + move) /
+   P(previous-moves)."
+  (let* ((n-gram (append previous-moves (list move)))
+         (n-gram-count (gethash n-gram count-map))
+         (previous-count (gethash previous-moves count-map)))
+    (if (and n-gram-count previous-count)
+        (/ n-gram-count previous-count)
+        0)))
+
+(defun valid-moves (board whitep)
+  "Find all moves available to the player on the given board. Evaluate
+   to a list of moves in algebraic chess notation."
+  (remove-if (lambda (move)
+               (not (valid-move-p move board whitep)))
+             +all-moves+))
+
+(defun best-move (board whitep previous-moves count-map)
+  "Determine the most likely chess move from those available to the
+   player. Also give the move's associated probability as a second
+   value."
+  (let (best-move best-probability)
+    (loop for move in (valid-moves board whitep)
+       for probability = (move-probability move previous-moves count-map)
+       when (or (null best-move) (> probability best-probability))
+       do (setf best-move move best-probability probability))
+    (values best-move best-probability)))
